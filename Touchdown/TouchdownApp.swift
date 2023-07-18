57d97a3d47311a47fcf3e3321007777780ef3d9e
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-18 17:28:46 +0200

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Cristina on 2023-07-18.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

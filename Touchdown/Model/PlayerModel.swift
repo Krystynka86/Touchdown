2a6be5fb90707e02d7816457e6479ce34138638c
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-18 20:00:40 +0200

    create a PlayerModel

---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Cristina on 2023-07-18.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}

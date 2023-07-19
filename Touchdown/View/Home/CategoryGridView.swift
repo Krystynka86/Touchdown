55769284155c547265c7ddaf5930e3e5d0e16d92
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-19 18:57:51 +0200

    create category grid

---
+++ Touchdown/View/Home/CategoryGridView.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryGridView.swift
+//  Touchdown
+//
+//  Created by Cristina on 2023-07-19.
+//
+
+import SwiftUI
+
+struct CategoryGridView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHGrid(rows: gridlayout, alignment: .center, spacing: columnSpacing, pinnedViews: [], content: {
+                ForEach(categories) { category in
+                    CategoryItemView(category: category)
+                }
+            }) //: GRID
+            .frame(height: 140)
+            .padding(.horizontal, 15)
+            .padding(.vertical, 10)
+        }) //: SCROLL
+    }
+}
+
+// MARK: - PREVIEW
+struct CategoryGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryGridView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(colorBackground)
+    }
+}

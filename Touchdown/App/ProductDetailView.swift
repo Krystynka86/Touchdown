cf3dcc8dc0b27cffecde540a575432dcf718aef1
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-23 22:01:31 +0200

    create top part deatail view

--- Touchdown/App/ProductDetailView.swift
+++ Touchdown/App/ProductDetailView.swift
@@ -20,7 +20,8 @@ struct ProductDetailView: View {
                          UIApplication.shared.windows.first?.safeAreaInsets.top)
             
             // HEADER
-            Text(sampleProduct.name)
+            HeaderDetailView()
+                .padding(.horizontal)
             // DETAIL TOP PART
             // DETAIL BOTTOM PART
             // RATINGS + SIZES

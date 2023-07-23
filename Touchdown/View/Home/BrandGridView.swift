cecd3ad339658dd03ffb93d5efba727ac7c76a23
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-23 20:49:49 +0200

    create Brand grid view

---
+++ Touchdown/View/Home/BrandGridView.swift
@@ -0,0 +1,35 @@
+//
+//  BrandGridView.swift
+//  Touchdown
+//
+//  Created by Cristina on 2023-07-23.
+//
+
+import SwiftUI
+
+struct BrandGridView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false, content:  {
+            LazyHGrid(rows: gridlayout, spacing: columnSpacing, content: {
+                ForEach(brands) { brand in
+                   BrandItemView(brand: brand)
+                }
+            })
+            .frame(height: 200)
+            .padding(15)
+        }) //: SCROLL
+    }
+}
+
+
+// MARK: - PREVIEW
+struct BrandGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        BrandGridView()
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}

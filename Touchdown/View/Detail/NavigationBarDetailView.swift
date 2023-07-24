2f18ec271b32de0837a4cea0cc45cd76e33a9144
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-07-24 09:54:20 +0200

    add logic to back button on Navigation Bar

--- Touchdown/View/Detail/NavigationBarDetailView.swift
+++ Touchdown/View/Detail/NavigationBarDetailView.swift
@@ -10,10 +10,17 @@ import SwiftUI
 struct NavigationBarDetailView: View {
     // MARK: - PROPERTIES
     
+    @EnvironmentObject var shop: Shop
+    
     // MARK: - BODY
     var body: some View {
         HStack {
-            Button(action: {}, label: {
+            Button(action: {
+                withAnimation(.easeOut) {
+                    shop.showingProduct = false
+                    shop.selectedProduct = nil
+                }
+            }, label: {
                 Image(systemName: "chevron.left")
                     .font(.title)
                     .foregroundColor(.white)
@@ -34,6 +41,7 @@ struct NavigationBarDetailView: View {
 struct NavigationBarDetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationBarDetailView()
+            .environmentObject(Shop())
             .previewLayout(.sizeThatFits)
             .padding()
             .background(Color.gray)
